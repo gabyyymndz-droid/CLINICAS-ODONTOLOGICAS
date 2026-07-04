47299b74ff6074d5fe63e93792fd654c8a4049e0
GGaboBD <007007gabo7u7@gmail.com>
2026-07-04 10:59:28 -0600

    COMMIT 2: agregacion de Modulo 'rol'

---
+++ Modulos/ROL.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+class Rol:
+    def __init__(self, idRol: str, nombreRol: str, Descripcion: str):
+        self.idRol = idRol
+        self.nombreRol = nombreRol
+        self.Descripcion = Descripcion
+
+    def __eq__(self, otro) -> bool:
+        if not isinstance(otro, Rol): 
+            return NotImplementedError("")
+        return self.idRol == otro.idRol
+
+    def validacion_rol(self, lista_roles: list) -> list:
+        errores = []
+
+        if not self.idRol or not str(self.idRol).strip():
+            errores.append("El ID del Rol no puede estar vacio")
+        if self in lista_roles: 
+            errores.append(f"El rol {self.idRol} ya existe en el sistema")
+
+        return errores
